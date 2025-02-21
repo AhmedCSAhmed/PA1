a7acec967b61936d04201c4c74146c08e7cf964a
mulupuruvikas <vikas.mulupuru@gmail.com>
2025-02-21 16:49:57 -0600

    added compute thrift

---
+++ compute.thrift
@@ -0,0 +1,10 @@
+struct Model {
+    1: list<list<double>> W
+    2: list<list<double>> V
+}
+
+service Compute {
+    Model get_gradient(),
+    void train(1:string training_file),
+    void set_model(1:Work shared_model)
+}
